26fa59e148c3dfd248a94ba4a34e83c372044f02
djsaer <107787842+djsaer@users.noreply.github.com>
2022-06-19 17:45:25 +0800

    Update Hash_Function.py

--- Hash_Function.py
+++ Hash_Function.py
@@ -1,6 +1,11 @@
-def fun():
-    print(hash('hello'))
-    print(hash(str([1,2,3,])))
-    print(hash(1))
+import base64
+import hashlib
+
 def main():
-    fun()
+    BeforeHash = input('请输入想要加密的字符串\n')
+    Mymd5 = hashlib.md5()
+    Mymd5.update(BeforeHash.encode('utf-8'))
+    hash_in_bytes = Mymd5.digest()
+    result = base64.b32encode(hash_in_bytes)
+    result = result.strip(b'=')
+    print(result)
